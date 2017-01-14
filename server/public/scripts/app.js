e9f3f63474e0c141d739078acf65709611a29731
Jame Jacobs-Smith <jakejacobs-smith1@Jakes-MacBook-Air.local>
2017-01-14 12:14:43 -0600

    initial commit

--- server/public/scripts/app.js
+++ server/public/scripts/app.js
@@ -1,9 +1,47 @@
 $(document).ready(function(){
-    $.ajax({
-      type: "GET",
-      url: "/data",
-      success: function(data){
-        console.log(data);
+  var displayedStudent = 0;
+  // var people = {};
+
+
+  $('.people').on('click', function() {
+    var index = $(this).attr('id');
+    currentStudent = Number(index - 1);
+    console.log(index);
+    console.log(currentStudent);
+
+    showStudent(currentStudent);
+
+    function showStudent(currentStudent) {
+      $.ajax({
+        type: "GET",
+        url: "/data",
+        success: function(data){
+          console.log("GET /data returns", data);
+          $('#stage').fadeOut('slow');
+          $('#stage').empty();
+          displayedStudent = data[currentStudent];
+          console.log(displayedStudent);
+          $('#stage').fadeIn('slow');
+          appendDom(displayedStudent);
+          }
+        });
       }
     });
+
+  // $('previousButton').on('click', currentStudent, function() {
+  //   $(currentStudent).fadeOut("slow");
+  //   currentStudent--;
+  // })
+
 });
+
+
+
+function appendDom(data){
+  var $studentDiv = $('<div class="student"></div>');
+  $studentDiv.append('<h2>' + data.name + '</h2>');
+  $studentDiv.append('<p>' + data.githubUserName + '</p>');
+  $studentDiv.append('<p>' + data.shoutout + '</p>');
+
+  $('#stage').append($studentDiv);
+};
